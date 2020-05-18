d13a97f54fcb7772e84c3f3ed48f4728e9032cc5
Rashed <r.sarwar87@gmail.com>
2020-05-18 23:50:47 +0100

    minor fix

--- server/server.mk
+++ server/server.mk
@@ -52,7 +52,7 @@ DEP := $(subst .o,.d,$(OBJ))
 SERVER_CCXX = /usr/bin/$(GCC_ARCH)-g++-5 -flto
 
 SERVER_CCXXFLAGS := -Wall -Werror -Wextra -static-libstdc++
-SERVER_CCXXFLAGS += -Wpedantic -Wfloat-equal -Wunused-macros -Wcast-qual -Wuseless-cast
+SERVER_CCXXFLAGS += -Wpedantic -Wfloat-equal -Wunused-macros -Wuseless-cast
 SERVER_CCXXFLAGS += -Wlogical-op -Wdouble-promotion -Wformat -Wmissing-include-dirs -Wundef
 SERVER_CCXXFLAGS +=  -Wpacked -Wredundant-decls -Wvarargs -Wvector-operation-performance -Wswitch-default
 SERVER_CCXXFLAGS += -Wuninitialized  -Wmissing-declarations
