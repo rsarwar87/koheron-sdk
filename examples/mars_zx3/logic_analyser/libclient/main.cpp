cdd6e4d16f43ca941d5b9de2ca8e173935024aea
R Sarwar <r.sarwar87@gmail.com>
2020-06-03 17:14:17 +0100

    logic analyser - added example

--- examples/mars_zx3/logic_analyser/libclient/main.cpp
+++ examples/mars_zx3/logic_analyser/libclient/main.cpp
@@ -1,11 +1,30 @@
-#include "log.hpp"
-#include <iostream>
+#include "logic_analyzer.hpp"
+#include "bitset"
+
 
 int main(){
- std::cout << "Hello World" << std::endl;
-syslog_stream clog;
 
-clog << "Hello, world!" << std::endl;
-clog << log::emergency << "foo" << "bar" << "baz" << 42 << std::endl;
- return 0;
+    logic_analyser_window ip("192.168.1.114", 36000);
+    ip.get_forty_two();
+
+    ip.set_triggers(0xfffffff);
+    ip.get_triggers();
+    auto data = ip.get_data();
+
+    for (size_t i = 0; i < 33; i++)
+    {
+      uint16_t *parr16 = (uint16_t*)(&(data[i]));
+      for (size_t k = 0; k < 2; k++){
+
+      std::bitset<16> bits(parr16[k]);
+      // Storing integral values in the string:
+      for(auto j: bits.to_string(char(0), char(1))) {
+        std::cout << "\t" << (int)j;
+      }
+      std::cout << std::endl;
+      }
+    }
+
+
+    std::cout << "\nhelloworld" << std::endl;
 }
