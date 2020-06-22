1fd0ffd126fc440fba26e1c1e931b8d2dd50bd1a
Rashed <r.sarwar87@gmail.com>
2020-06-22 22:25:08 +0100

    Update board OS design

--- boards/mercury_ux1_pe1/board.mk
+++ boards/mercury_ux1_pe1/board.mk
@@ -1,5 +1,5 @@
 # Linux and U-boot
-UBOOT_CONFIG = zynq_mercury_ux1_defconfig 
+UBOOT_CONFIG = enclustra_zynqmp_mercury_xu1_defconfig  
 ZYNQ_TYPE ?= zynqmp
 DTREE_OVERRIDE = arch/arm64/boot/dts/xilinx/zynqmp-enclustra-xu1.dtb
 DTREE_LOC = linux  
@@ -8,6 +8,8 @@ LINUX_TAG := enclustra-linux-v$(VIVADO_VERSION)
 DTREE_TAG := xilinx-v$(VIVADO_VERSION)
 ZYNQ_TYPE := zynqmp
 
+BOOT_MEDIUM := mmcblk1
+
 UBOOT_URL := https://github.com/enclustra-bsp/xilinx-uboot/archive/v1.8.2.tar.gz
-LINUX_URL := https://github.com/enclustra-bsp/xilinx-linux/archive/v1.8.2.tar.gz
-DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
+LINUX_URL := https://github.com/rsarwar87/enclustra_kernel_xilinx/archive/2020.1-2.tar.gz
+DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/xilinx-v2020.1.tar.gz
