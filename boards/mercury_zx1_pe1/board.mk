2bd9a78487aaf39c957fe5f63c23d39b628cfeb4
Rashed <r.sarwar87@gmail.com>
2020-06-27 01:32:53 +0100

    restore support for zx1 for vivado 2020.1

--- boards/mercury_zx1_pe1/board.mk
+++ boards/mercury_zx1_pe1/board.mk
@@ -2,10 +2,10 @@
 UBOOT_CONFIG = zynq_pe1_zx1_defconfig
 DTREE_OVERRIDE = arch/arm/boot/dts/zynq-mercury-zx1.dtb
 DTREE_LOC = linux  
-UBOOT_TAG := enclustra-uboot-v$(VIVADO_VERSION)
-LINUX_TAG := enclustra-linux-v$(VIVADO_VERSION)
+UBOOT_TAG := enclustra-uboot-v1.8.tar.gz
+LINUX_TAG := xilinx-linux-v$(VIVADO_VERSION)
 DTREE_TAG := xilinx-v$(VIVADO_VERSION)
 
 UBOOT_URL := https://github.com/enclustra-bsp/xilinx-uboot/archive/v1.8.tar.gz
-LINUX_URL := https://github.com/enclustra-bsp/xilinx-linux/archive/v1.8.2.tar.gz
-DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
+LINUX_URL := https://github.com/Xilinx/linux-xlnx/archive/xilinx-v$(VIVADO_VERSION).tar.gz
+TREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/xilinx-v2020.1.tar.gz
