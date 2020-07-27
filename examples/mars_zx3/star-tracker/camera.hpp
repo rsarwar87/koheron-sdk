faea845e940845b730d0ee32d1ca1f58b6759ed0
Rashed <r.sarwar87@gmail.com>
2020-07-28 00:06:49 +0100

    update interface for camera trigger

--- examples/mars_zx3/star-tracker/camera.hpp
+++ examples/mars_zx3/star-tracker/camera.hpp
@@ -13,16 +13,16 @@ class CameraInterface {
   SkyTrackerInterface(Context& ctx_)
       : ctx(ctx_),
         ctl(ctx.mm.get<mem::control>()),
-        sts(ctx.mm.get<mem::status>()),
   {
   }
 
   bool set_cameratrigger_reg(uint8_t val){
     ctl.write<reg::camera_trigger>(val);
     ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, val);
+    return true;
   }
   uint8_t get_cameratrigger_reg(){
-    uint32_t ret = sts.read<reg::camera_trigger>();
+    uint32_t ret = ctl.read<reg::camera_trigger>();
     ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, ret);
     return ret; 
   }
@@ -30,15 +30,14 @@ class CameraInterface {
     ctl.write<reg::camera_trigger>(0x1);
     std::this_thread::sleep_for(100ms);
     ctl.write<reg::camera_trigger>(0x3);
-    uint32_t ret = sts.read<reg::camera_trigger>();
-    ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, ret);
+    ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, ctl.read<reg::camera_trigger>());
     return true;
   }
   bool close_shutter(){
     ctl.write<reg::camera_trigger>(0x1);
     std::this_thread::sleep_for(100ms);
     ctl.write<reg::camera_trigger>(0x0);
-    ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, sts.read<reg::camera_trigger>());
+    ctx.log<INFO>("CameraInterface: %s=0x%08x\n", __func__, ctl.read<reg::camera_trigger>());
     return true;
   }
 
@@ -46,9 +45,6 @@ class CameraInterface {
  private:
   Context& ctx;
   Memory<mem::control>& ctl;
-  Memory<mem::status>& sts;
-
-
 };
 
 #endif  // __DRIVERS_LED_BLINKER_HPP__
