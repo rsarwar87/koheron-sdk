31a2ecea32e3415ab72ba85daf677f8d2133d0e4
Rashed <r.sarwar87@gmail.com>
2020-10-25 01:14:23 +0100

    update microzed support

---
+++ boards/microzed/board.mk
@@ -0,0 +1,12 @@
+# Linux and U-boot
+UBOOT_CONFIG = zynq_microzed_defconfig  
+ZYNQ_TYPE = zynq
+VIVADO_VER = 2020.1
+VIVADO_VER2 = 2020.1
+UBOOT_TAG := xilinx-uboot-v$(VIVADO_VER)
+LINUX_TAG := xilinx-linux-v$(VIVADO_VERSION)
+DTREE_TAG := xilinx-v$(VIVADO_VER)
+
+UBOOT_URL := https://github.com/Xilinx/u-boot-xlnx/archive/xilinx-v$(VIVADO_VER).1.tar.gz
+LINUX_URL := https://github.com/Xilinx/linux-xlnx/archive/xilinx-v$(VIVADO_VERSION).tar.gz
+DTREE_URL := https://github.com/Xilinx/device-tree-xlnx/archive/$(DTREE_TAG).tar.gz
