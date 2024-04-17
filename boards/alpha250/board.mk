6386bc6e1e8b94802daded20e2428564402939ad
rashed <rashed.sarwar@ukaea.uk>
2024-04-17 13:08:47 +0100

    test for version

--- boards/alpha250/board.mk
+++ boards/alpha250/board.mk
@@ -3,7 +3,10 @@ UBOOT_CONFIG = zynq_alpha250_defconfig
 ZYNQ_TYPE ?= zynq
 UBOOT_TAG := xilinx-uboot-v$(VIVADO_VERSION)
 LINUX_TAG := xilinx-linux-v$(VIVADO_VERSION)
-DTREE_TAG := xilinx_v$(VIVADO_VERSION)
+DTREE_TAG := xilinx-v$(VIVADO_VERSION)
+ifeq ($(shell echo "$(VIVADO_VERSION) > 2021.0" | bc), 1)
+   DTREE_TAG := xilinx_v$(VIVADO_VERSION)
+endif
 
 UBOOT_URL := https://github.com/Xilinx/u-boot-xlnx/archive/xilinx-v$(VIVADO_VERSION).tar.gz
 LINUX_URL := https://github.com/Xilinx/linux-xlnx/archive/refs/tags/xilinx-v$(VIVADO_VERSION).tar.gz
